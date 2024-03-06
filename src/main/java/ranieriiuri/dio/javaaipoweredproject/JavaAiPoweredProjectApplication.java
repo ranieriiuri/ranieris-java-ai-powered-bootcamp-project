c7b396f0493b49bdd3b249c02f7cba356d0ffb15
ranieriiuri <iuriranierioliveira@gmail.com>
2024-03-06 11:24:19 -0300

    first commit

---
+++ src/main/java/ranieriiuri/dio/javaaipoweredproject/JavaAiPoweredProjectApplication.java
@@ -0,0 +1,13 @@
+package ranieriiuri.dio.javaaipoweredproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAiPoweredProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAiPoweredProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ranieriiuri/dio/javaaipoweredproject/JavaAiPoweredProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ranieriiuri.dio.javaaipoweredproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAiPoweredProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
